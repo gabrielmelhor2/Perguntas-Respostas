74f5d9fdf5fe2d79f855c3456f72612f8ac99507
Gabriel Melhor <melhorgabriel11@gmail.com>
2022-11-25 09:47:10 -0300

    Adição de mais perguntas

--- app.py
+++ app.py
@@ -4,6 +4,16 @@ perguntas = [
         "Pergunta": "Quanto é 2+2?",
         "Opções": ["1", "3", "4", "5"],
         "Resposta":  "4",
+    },
+    {
+        "Pergunta": "Quanto é 5*5?",
+        "Opções": ["58", "49", "13", "25"],
+        "Resposta": "25"
+    },
+    {
+        "Pergunta": "Quanto é 10/2?",
+        "Opções": ["5", "98", "0", "7"],
+        "Resposta": "5"
     }
 ]
 
